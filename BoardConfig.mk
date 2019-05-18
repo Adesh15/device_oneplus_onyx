c1528d88e5991ed4dff70b16e61e4e2409e8fc32
Adesh15 <adesikha15@gmail.com>
2019-05-18 14:23:55 +0530

    onyx: Cleanup
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,8 +94,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(PLATFORM_PATH)/charger/images
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
-# CM Hardware
-JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(PLATFORM_PATH)/lineagehw|**/*.java
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Enable dexpreopt to speed boot time
